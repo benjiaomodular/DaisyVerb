eb4e2ee5cff26704b232e7e6458147c601e3c36b
Benjie Jiao <hi@benjie.me>
2022-09-11 03:50:33 +0800

    Added mode knobs and stuff

--- Software/DaisyVerb/DaisyVerb.ino
+++ Software/DaisyVerb/DaisyVerb.ino
@@ -8,38 +8,45 @@
 DaisyHardware hw;
 
 size_t num_channels;
+float sample_rate;
 
 ReverbSc DSY_SDRAM_BSS verb;
 PitchShifter DSY_SDRAM_BSS ps;
+
 static Jitter jitter;
+SampleRateReducer sr;
+
+Overdrive drive;
 
 static float dryLevel, wetLevel, send;
-float jitterMix;
+float jitterMixKnob;
+float verbMixKnob;
+float srKnob;
 
 
-float CtrlVal(uint8_t pin) { return (analogRead(pin)) / 1023.f; }
+float CtrlVal(uint8_t pin) {
+  analogReadResolution(16);
+  return (analogRead(pin)) / 65535.f;
+}
 
 void MyCallback(float **in, float **out, size_t size) {
   float dryL, dryR, verbL, verbR;
-  float shifted;
+  float crushed;
   float jitter_out;
 
   for (size_t i = 0; i < size; i++) {
     dryL = in[0][i];
     dryR = in[1][i];
 
-    jitter_out = jitter.Process();
- 
+    jitter_out = jitter.Process();  
     verb.Process(dryL, dryR, &verbL, &verbR);
-    shifted = ps.Process(verbL);
-
-    out[0][i] = (dryL * dryLevel) + shifted * ((1-jitterMix) + (jitter_out * jitterMix));
+  
+    out[0][i] = (dryL * dryLevel) + verbL * ((1-jitterMixKnob) + (jitter_out * jitterMixKnob)) * verbMixKnob;
     out[1][i] = (dryR * dryLevel) + verbR;
   }
 }
 
 void setup() {
-  float sample_rate;
   
   // Initialize for Daisy pod at 48kHz
   hw = DAISY.init(DAISY_SEED, AUDIO_SR_48K);
@@ -51,14 +58,12 @@ void setup() {
   verb.SetFeedback(0.95f);
   verb.SetLpFreq(18000.0f);
 
-  // Initialize pitch shifter
-  ps.Init(sample_rate);
-  ps.SetTransposition(.0f);
-
   jitter.Init(sample_rate);
   jitter.SetAmp(1);
   jitter.SetCpsMin(1);
-  jitter.SetCpsMax(30);
+  jitter.SetCpsMax(25);
+
+  sr.Init();
 
   wetLevel = 0.1f;
   DAISY.begin(MyCallback);
@@ -66,12 +71,11 @@ void setup() {
 
 void loop() {
   dryLevel = CtrlVal(A0);
-  verb.SetFeedback(.50f + CtrlVal(A1) * .49f);
-  
-  // Minimum offset a workaround for weird noise when set to 0
-  ps.SetTransposition(0.000001 + CtrlVal(A2) * 12.0f);
-
-  jitterMix = CtrlVal(A3);
+  verb.SetFeedback(0.75f + CtrlVal(A1) * .2499f);
+ 
+  verbMixKnob = CtrlVal(A2);
+  jitterMixKnob = CtrlVal(A3);
+ 
   verb.SetLpFreq(CtrlVal(A4) * 20000.0f);
 
 }
